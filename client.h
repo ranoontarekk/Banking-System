35d8bbff84328fb4f0bb4ea2091ec01ed32f0767
mohamedmahmoudelgendy <103971845+mohamedmahmoudelgendy@users.noreply.github.com>
2022-11-07 00:55:16 +0200

    still need to edit account variable

---
+++ client.h
@@ -0,0 +1,16 @@
+class client : public BankApplication{
+    private:
+        string name ;
+        string address ;
+        string phone ;
+        //string account ; 
+    public:
+        void set_name(string n);
+        string getname();
+        void set_address(string d);
+        string getaddress();
+        void set_phone(string p);
+        string get_phone();
+        // void setaccount(string A);
+        // string get_account();
+};
